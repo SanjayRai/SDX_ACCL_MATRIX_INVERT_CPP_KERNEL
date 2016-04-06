906e3893e37e0bebf724418c4135568c6c94b68b
Sanjay Rai <sanjayr@MC32GBLNX.(none)>
2016-04-06 13:20:10 -0700

    Added better test bench matric creation

--- src/getCPUTime.cpp
+++ src/getCPUTime.cpp
@@ -1,9 +1,3 @@
-/*
- * Author:  David Robert Nadeau
- * Site:    http://NadeauSoftware.com/
- * License: Creative Commons Attribution 3.0 Unported License
- *          http://creativecommons.org/licenses/by/3.0/deed.en_US
- */
 #if defined(_WIN32)
 #include <Windows.h>
 
